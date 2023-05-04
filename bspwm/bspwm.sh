955bb5a0f20a8c77e02c6e2f86e8d19d9350877b
Sherman Rofeman <skippyr.developer@gmail.com>
2023-05-04 11:32:09 -0300

    refactor: add fresh start to work with new colors

--- bspwm/bspwm.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-bspc config focused_border_color "#b01002"
-
--- dwm/dwm.c
+++
@@ -1,6 +0,0 @@
-static const char col_gray1[]       = "#0d0501";
-static const char col_gray2[]       = "#b01002";
-static const char col_gray3[]       = "#faedac";
-static const char col_gray4[]       = "#faedac";
-static const char col_cyan[]        = "#b01002";
-
