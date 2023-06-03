2fdf89c3d3c7e483d809382d57eda54c204fa727
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-03 17:20:41 -0300

    refactor: change colors of linux virtual console theme

--- linux_virtual_console/flamerial.sh
+++ linux_virtual_console/flamerial.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 if [[ ${TERM} == "linux" ]]; then
-	echo -en "\\e]P00d0600"
-	echo -en "\\e]P1a62126"
-	echo -en "\\e]P2755717"
-	echo -en "\\e]P3a86419"
-	echo -en "\\e]P48c6f46"
-	echo -en "\\e]P5754636"
-	echo -en "\\e]P6852d10"
+	echo -en "\\e]P0170e05"
+	echo -en "\\e]P1b3001b"
+	echo -en "\\e]P29c661f"
+	echo -en "\\e]P3bf5b04"
+	echo -en "\\e]P4335c58"
+	echo -en "\\e]P5744d34"
+	echo -en "\\e]P69c3513"
 	echo -en "\\e]P7f5cc7a"
 	echo -en "\\e]P8916634"
 	echo -en "\\e]P9a62126"
