a10b4cd5cdc2c10b811d6b6cb443313aca6f86c2
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-27 14:10:05 -0300

    feat: add port to bspwm

---
+++ scripts/ports/bspwm.rb
@@ -0,0 +1,11 @@
+require_relative("../libs/metadata")
+
+def print_config(option, color)
+  puts("bspc config #{option} \"#{color}\"")
+end
+
+puts("#!/usr/bin/env bash")
+print_config("focused_border_color", $flamerial[:colors][:red])
+print_config("normal_border_color", $flamerial[:colors][:black])
+print_config("presel_feedback_color", $flamerial[:colors][:magenta])
+
