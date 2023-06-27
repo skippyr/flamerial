1f4f9d79ecf47ab1014a0eab2817087d2db11ac1
Sherman Rofeman <skippyr.developer@gmail.com>
2023-06-27 12:41:04 -0300

    feat: add theme for linux console

---
+++ linux/flamerial.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+[[ ${TERM} != "linux" ]] && exit
+echo -en "\\e]P0241a12"
+echo -en "\\e]P1a62929"
+echo -en "\\e]P29a6b1b"
+echo -en "\\e]P3d97904"
+echo -en "\\e]P4a59a78"
+echo -en "\\e]P5b3374a"
+echo -en "\\e]P6c2421f"
+echo -en "\\e]P7e6d0bb"
+echo -en "\\e]P8c78e59"
+echo -en "\\e]P9a62929"
+echo -en "\\e]Pa9a6b1b"
+echo -en "\\e]Pbd97904"
+echo -en "\\e]Pca59a78"
+echo -en "\\e]Pdb3374a"
+echo -en "\\e]Pec2421f"
+echo -en "\\e]Pfe6d0bb"
