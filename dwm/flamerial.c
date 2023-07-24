ae1718270772992f72b639dd4d8deb8712ca75f1
Sherman Rofeman <skippyr.developer@gmail.com>
2023-07-24 10:29:12 -0300

    ~% Use tabs for dwm theme.

--- dwm/flamerial.c
+++ dwm/flamerial.c
@@ -1,3 +1,3 @@
 static const char
-    col_gray1[] = "#140f0f", col_gray2[] = "#140f0f", col_gray3[] = "#a88e6e",
-    col_gray4[] = "#e8dcbc", col_cyan[] = "#960c0c";
+	col_gray1[] = "#140f0f", col_gray2[] = "#140f0f", col_gray3[] = "#a88e6e",
+	col_gray4[] = "#e8dcbc", col_cyan[] = "#960c0c";
