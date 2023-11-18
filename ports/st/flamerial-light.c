bc1c8d03a8dab5fb19eb2ce2a9862afa2037aff9
Sherman Rofeman <skippyr.developer@gmail.com>
2023-11-18 20:06:41 +0000

    add ports for st

---
+++ ports/st/flamerial-light.c
@@ -0,0 +1,23 @@
+static const char *colorname[] = {
+	"#e9d397",
+	"#b31c13",
+	"#990101",
+	"#c76800",
+	"#b08548",
+	"#8a3820",
+	"#9c3900",
+	"#150d06",
+	"#b08548",
+	"#b31c13",
+	"#990101",
+	"#c76800",
+	"#b08548",
+	"#8a3820",
+	"#9c3900",
+	"#150d06"
+};
+
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 0;
