329bb398794db3457965fb70a42d8e799fa958f2
Sherman Rofeman <skippyr.developer@gmail.com>
2023-12-12 12:46:23 -0300

    add linux-console theme

---
+++ linux-console/flamerial.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo -ne "\\e]P0150d06"
+echo -ne "\\e]P1b31c13"
+echo -ne "\\e]P2990101"
+echo -ne "\\e]P3c76800"
+echo -ne "\\e]P4b08548"
+echo -ne "\\e]P58a3820"
+echo -ne "\\e]P69c3900"
+echo -ne "\\e]P7e9d397"
+echo -ne "\\e]P8b08548"
+echo -ne "\\e]P9b31c13"
+echo -ne "\\e]Pa990101"
+echo -ne "\\e]Pbc76800"
+echo -ne "\\e]Pcb08548"
+echo -ne "\\e]Pd8a3820"
+echo -ne "\\e]Pe9c3900"
+echo -ne "\\e]Pfe9d397"
