92668edad853e1863328588b990e56f02f722b24
Sherman Rofeman <skippyr.developer@gmail.com>
2024-01-24 12:54:32 -0300

    update preview

--- preview.c
+++ preview.c
@@ -4,12 +4,13 @@ void clr(int lyr);
 void ln(void);
 void title(void);
 
-void clr(int lyr)
+void
+clr(int lyr)
 {
 	int i;
 	tdk_setclr(tdk_ClrBle, tdk_LyrFg);
 	printf("¦ ");
-	for (i = 1; i < 8; i++) {
+	for (i = 1; i < 8; i++)
 		if (lyr == tdk_LyrFg) {
 			tdk_setclr(i, tdk_LyrFg);
 			printf("Fla");
@@ -17,14 +18,14 @@ void clr(int lyr)
 			tdk_setclr(i, tdk_LyrBg);
 			printf("   ");
 		}
-	}
 	tdk_setclr(tdk_ClrDft, tdk_LyrBg);
 	tdk_setclr(tdk_ClrBle, tdk_LyrFg);
 	printf(" ¦");
 	tdk_setclr(tdk_ClrDft, tdk_LyrFg);
 }
 
-void ln(void)
+void
+ln(void)
 {
 	int i;
 	tdk_setclr(tdk_ClrYlw, tdk_LyrFg);
@@ -32,9 +33,8 @@ void ln(void)
 	tdk_setclr(tdk_ClrRed, tdk_LyrFg);
 	printf("~");
 	tdk_setclr(tdk_ClrBle, tdk_LyrFg);
-	for (i = 0; i < 21; i++) {
+	for (i = 0; i < 21; i++)
 		putchar('-');
-	}
 	tdk_setclr(tdk_ClrRed, tdk_LyrFg);
 	printf("~");
 	tdk_setclr(tdk_ClrYlw, tdk_LyrFg);
@@ -42,7 +42,8 @@ void ln(void)
 	tdk_setclr(tdk_ClrDft, tdk_LyrFg);
 }
 
-void title(void)
+void
+title(void)
 {
 	int ccol;
 	int cln;
@@ -52,7 +53,8 @@ void title(void)
 	tdk_setcpos(ccol, cln);
 }
 
-int main(void)
+int
+main(void)
 {
 	printf("           🔥🐉\n");
 	ln();
@@ -64,5 +66,5 @@ int main(void)
 	printf("\n");
 	ln();
 	printf("\n");
-	return (0);
+	return 0;
 }
