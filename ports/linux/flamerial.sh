75da24c2d024f7c56ab0861e6d303d17a094533d
Sherman Rofeman <skippyr.developer@gmail.com>
2024-03-09 00:26:54 -0300

    prepare v6.0.0
    - Adjust brightness of green, magenta and cyan colors.

--- ports/linux/flamerial.sh
+++ ports/linux/flamerial.sh
@@ -1,27 +1,29 @@
 #!/bin/sh
 
-set_color() {
-  printf "\033]P${1}${2}";
+function setColor
+{
+	printf "\033]P${1}${2}";
 }
 
-if [[ ${TERM} != linux ]]; then
-  printf 'flamerial.sh: the linux terminal is not being used.\n';
-  exit 1;
+if [[ ${TERM} != linux ]];
+then
+	printf "flamerial.sh: the linux terminal is not being used.\n";
+	exit 1;
 fi
 
-set_color 0 24130e;
-set_color 1 b8150d;
-set_color 2 9e7100;
-set_color 3 db7a00;
-set_color 4 c48d49;
-set_color 5 9c542d;
-set_color 6 80854b;
-set_color 7 e8cf89;
-set_color 8 c48d49;
-set_color 9 b8150d;
-set_color a 9e7100;
-set_color b db7a00;
-set_color c c48d49;
-set_color d 9c542d;
-set_color e 80854b;
-set_color f e8cf89;
+setColor 0 24130e;
+setColor 1 b8150d;
+setColor 2 a1650b;
+setColor 3 db7a00;
+setColor 4 c48d49;
+setColor 5 9e4711;
+setColor 6 948141;
+setColor 7 e8cf89;
+setColor 8 c48d49;
+setColor 9 b8150d;
+setColor a a1650b;
+setColor b db7a00;
+setColor c c48d49;
+setColor d 9e4711;
+setColor e 948141;
+setColor f e8cf89;
