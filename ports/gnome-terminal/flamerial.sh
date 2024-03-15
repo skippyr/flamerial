c39833fca78efd4f667c20e1e617d91545248b71
Sherman Rofeman <skippyr.developer@gmail.com>
2024-03-14 21:30:34 -0300

    update shell script formatting

--- ports/gnome-terminal/flamerial.sh
+++ ports/gnome-terminal/flamerial.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-typeset -g g_profileUUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
+typeset -g g_profileUUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \');
 
 function applyProperty()
 {
-    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${g_profileUUID}/ ${1} ${2}
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${g_profileUUID}/ ${1} ${2};
 }
 
-applyProperty use-theme-colors false
-applyProperty background-color \#24130e
-applyProperty foreground-color \#e8cf89
-applyProperty cursor-background-color \#e8cf89
-applyProperty cursor-foreground-color \#24130e
-applyProperty highlight-background-color \#e8cf89
-applyProperty highlight-foreground-color \#24130e
-applyProperty palette [\"#24130e\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#948141\",\"#e8cf89\",\"#c48d49\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#948141\",\"#e8cf89\"]
+applyProperty use-theme-colors false;
+applyProperty background-color \#24130e;
+applyProperty foreground-color \#e8cf89;
+applyProperty cursor-background-color \#e8cf89;
+applyProperty cursor-foreground-color \#24130e;
+applyProperty highlight-background-color \#e8cf89;
+applyProperty highlight-foreground-color \#24130e;
+applyProperty palette [\"#24130e\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#948141\",\"#e8cf89\",\"#c48d49\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#948141\",\"#e8cf89\"];
