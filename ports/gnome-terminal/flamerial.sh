ce0f98d3fb508a3f0f80a9431e434799ee780121
Sherman Rofeman <skippyr.developer@gmail.com>
2024-04-02 11:42:20 -0300

    refactor shell formatting

--- ports/gnome-terminal/flamerial.sh
+++ ports/gnome-terminal/flamerial.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-PROFILE_UUID=$(gsettings get org.gnome.Terminal.ProfilesList default |
+profile_uuid_g=$(gsettings get org.gnome.Terminal.ProfilesList default |
                tr -d \');
 
-function apply_property {
+apply_property() {
   gsettings set \
-  org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${PROFILE_UUID}/ \
+  "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${profile_uuid_g}/" \
   ${1} ${2};
 }
 
 apply_property use-theme-colors false;
-apply_property background-color \#24130e;
-apply_property foreground-color \#e8cf89;
-apply_property cursor-background-color \#e8cf89;
-apply_property cursor-foreground-color \#24130e;
-apply_property highlight-background-color \#e8cf89;
-apply_property highlight-foreground-color \#24130e;
-apply_property palette [\"#24130e\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\
+apply_property background-color "#24130e";
+apply_property foreground-color "#e8cf89";
+apply_property cursor-background-color "#e8cf89";
+apply_property cursor-foreground-color "#24130e";
+apply_property highlight-background-color "#e8cf89";
+apply_property highlight-foreground-color "#24130e";
+apply_property palette "[\"#24130e\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\
 \"#c48d49\",\"#9e4711\",\"#948141\",\"#e8cf89\",\"#c48d49\",\"#b8150d\",\
-\"#a1650b\",\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#948141\",\"#e8cf89\"];
+\"#a1650b\",\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#948141\",\"#e8cf89\"]";
--- ports/linux/flamerial.sh
+++ ports/linux/flamerial.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function set_color {
+set_color() {
   printf "\033]P${1}${2}";
 }
 
@@ -9,19 +9,19 @@ if [[ ${TERM} != linux ]]; then
   exit 1;
 fi
 
-set_color 0 24130e;
-set_color 1 b8150d;
-set_color 2 a1650b;
-set_color 3 db7a00;
-set_color 4 c48d49;
-set_color 5 9e4711;
-set_color 6 948141;
-set_color 7 e8cf89;
-set_color 8 c48d49;
-set_color 9 b8150d;
-set_color a a1650b;
-set_color b db7a00;
-set_color c c48d49;
-set_color d 9e4711;
-set_color e 948141;
-set_color f e8cf89;
+set_color 0 "24130e";
+set_color 1 "b8150d";
+set_color 2 "a1650b";
+set_color 3 "db7a00";
+set_color 4 "c48d49";
+set_color 5 "9e4711";
+set_color 6 "948141";
+set_color 7 "e8cf89";
+set_color 8 "c48d49";
+set_color 9 "b8150d";
+set_color a "a1650b";
+set_color b "db7a00";
+set_color c "c48d49";
+set_color d "9e4711";
+set_color e "948141";
+set_color f "e8cf89";
