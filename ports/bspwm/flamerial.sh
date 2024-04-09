9bbe617cf110af9a3cbc76f9c9b05eb6c6c87a4c
Sherman Rofeman <skippyr.developer@gmail.com>
2024-04-09 14:38:24 -0300

    update tools

--- ports/bspwm/flamerial.sh
+++ ports/bspwm/flamerial.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bspc config active_border_color \#b8150d;
-bspc config focused_border_color \#b8150d;
-bspc config normal_border_color \#24130e;
-bspc config presel_feedback_color \#db7a00;
+bspc config active_border_color "#b8150d";
+bspc config focused_border_color "#b8150d";
+bspc config normal_border_color "#24130e";
+bspc config presel_feedback_color "#db7a00";
