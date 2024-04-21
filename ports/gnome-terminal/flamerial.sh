c5d560e65d3b90507f44f924a4fc20013d5b9555
Sherman Rofeman <skippyr.developer@gmail.com>
2024-04-21 19:05:12 -0300

    update shell syntax

--- ports/gnome-terminal/flamerial.sh
+++ ports/gnome-terminal/flamerial.sh
@@ -3,17 +3,17 @@
 uuid_g=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \');
 
 setattr() {
-	gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${uuid_g}/" \
+	gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${uuid_g}/ \
 				  ${1} ${2};
 }
 
 setattr use-theme-colors false;
-setattr background-color "#24130e";
-setattr foreground-color "#e8cf89";
-setattr cursor-background-color "#e8cf89";
-setattr cursor-foreground-color "#24130e";
-setattr highlight-background-color "#e8cf89";
-setattr highlight-foreground-color "#24130e";
-setattr palette "[\"#24130e\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\"#c48d49\",\
+setattr background-color \#24130e;
+setattr foreground-color \#e8cf89;
+setattr cursor-background-color \#e8cf89;
+setattr cursor-foreground-color \#24130e;
+setattr highlight-background-color \#e8cf89;
+setattr highlight-foreground-color \#24130e;
+setattr palette [\"#24130e\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\"#c48d49\",\
 \"#9e4711\",\"#948141\",\"#e8cf89\",\"#c48d49\",\"#b8150d\",\"#a1650b\",\
-\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#948141\",\"#e8cf89\"]";
+\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#948141\",\"#e8cf89\"];
--- ports/linux/flamerial.sh
+++ ports/linux/flamerial.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 setclr() {
-	echo -ne "\033]P${1}${2}";
+	echo -ne \\033]P${1}${2};
 }
 
-if [[ ${TERM} != "linux" ]];
+if [[ ${TERM} != linux ]];
 then
 	echo "flamerial.sh: the linux terminal is not being used.";
 	exit 1;
 fi
 
-setclr 0 "24130e";
-setclr 1 "b8150d";
-setclr 2 "a1650b";
-setclr 3 "db7a00";
-setclr 4 "c48d49";
-setclr 5 "9e4711";
-setclr 6 "948141";
-setclr 7 "e8cf89";
-setclr 8 "c48d49";
-setclr 9 "b8150d";
-setclr a "a1650b";
-setclr b "db7a00";
-setclr c "c48d49";
-setclr d "9e4711";
-setclr e "948141";
-setclr f "e8cf89";
+setclr 0 24130e;
+setclr 1 b8150d;
+setclr 2 a1650b;
+setclr 3 db7a00;
+setclr 4 c48d49;
+setclr 5 9e4711;
+setclr 6 948141;
+setclr 7 e8cf89;
+setclr 8 c48d49;
+setclr 9 b8150d;
+setclr a a1650b;
+setclr b db7a00;
+setclr c c48d49;
+setclr d 9e4711;
+setclr e 948141;
+setclr f e8cf89;
