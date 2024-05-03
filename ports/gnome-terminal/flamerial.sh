a20d15d28726cc4b38727fccf2f66e69e03bb383
Sherman Rofeman <skippyr.developer@gmail.com>
2024-05-03 10:10:23 -0300

    update formatting

--- ports/gnome-terminal/flamerial.sh
+++ ports/gnome-terminal/flamerial.sh
@@ -2,18 +2,18 @@
 
 uuid_g=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \');
 
-setattr() {
-	gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${uuid_g}/ \
-				  ${1} ${2};
+setAttribute()
+{
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${uuid_g}/\
+                  ${1} ${2};
 }
 
-setattr use-theme-colors false;
-setattr background-color \#24130e;
-setattr foreground-color \#e8cf89;
-setattr cursor-background-color \#e8cf89;
-setattr cursor-foreground-color \#24130e;
-setattr highlight-background-color \#e8cf89;
-setattr highlight-foreground-color \#24130e;
-setattr palette [\"#24130e\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\"#c48d49\",\
-\"#9e4711\",\"#c74216\",\"#e8cf89\",\"#c48d49\",\"#b8150d\",\"#a1650b\",\
-\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#c74216\",\"#e8cf89\"];
+setAttribute use-theme-colors false;
+setAttribute background-color \#24130e;
+setAttribute foreground-color \#e8cf89;
+setAttribute cursor-background-color \#e8cf89;
+setAttribute cursor-foreground-color \#24130e;
+setAttribute highlight-background-color \#e8cf89;
+setAttribute highlight-foreground-color \#24130e;
+setAttribute palette [\"#24130e\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#c74216\",\"#e8cf89\",\
+\"#c48d49\",\"#b8150d\",\"#a1650b\",\"#db7a00\",\"#c48d49\",\"#9e4711\",\"#c74216\",\"#e8cf89\"];
--- ports/linux/flamerial.sh
+++ ports/linux/flamerial.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-setclr() {
-	echo -ne \\033]P${1}${2};
+setColor()
+{
+    echo -ne \\033]P${1}${2};
 }
 
 if [[ ${TERM} != linux ]];
 then
-	echo "flamerial.sh: the linux terminal is not being used.";
-	exit 1;
+    echo "flamerial.sh: the linux terminal is not being used.";
+    exit 1;
 fi
 
-setclr 0 24130e;
-setclr 1 b8150d;
-setclr 2 a1650b;
-setclr 3 db7a00;
-setclr 4 c48d49;
-setclr 5 9e4711;
-setclr 6 c74216;
-setclr 7 e8cf89;
-setclr 8 c48d49;
-setclr 9 b8150d;
-setclr a a1650b;
-setclr b db7a00;
-setclr c c48d49;
-setclr d 9e4711;
-setclr e c74216;
-setclr f e8cf89;
+setColor 0 24130e;
+setColor 1 b8150d;
+setColor 2 a1650b;
+setColor 3 db7a00;
+setColor 4 c48d49;
+setColor 5 9e4711;
+setColor 6 c74216;
+setColor 7 e8cf89;
+setColor 8 c48d49;
+setColor 9 b8150d;
+setColor a a1650b;
+setColor b db7a00;
+setColor c c48d49;
+setColor d 9e4711;
+setColor e c74216;
+setColor f e8cf89;
