ed23bf2a2eb3e08f8cbbd76b5758a171350a548d
Sherman Rofeman <skippyr.developer@gmail.com>
2024-05-17 14:32:58 -0300

    update formatting

--- ports/gnome-terminal/flamerial.sh
+++ ports/gnome-terminal/flamerial.sh
@@ -2,20 +2,20 @@
 
 g_uuid=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \');
 
-function SetAttribute()
-{
-    gsettings set                                                                                  \
-        org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${g_uuid}/         \
-        ${1} ${2};
+set_attribute() {
+  gsettings set \
+      org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${g_uuid}/ \
+      ${1} ${2};
 }
 
-SetAttribute use-theme-colors false;
-SetAttribute background-color \#221107;
-SetAttribute foreground-color \#f5efcd;
-SetAttribute cursor-background-color \#f5efcd;
-SetAttribute cursor-foreground-color \#221107;
-SetAttribute highlight-background-color \#f5efcd;
-SetAttribute highlight-foreground-color \#221107;
-SetAttribute palette [\"#221107\",\"#990b3a\",\"#cc561b\",\"#e0a100\",\"#b98361\",\"#ae3a34\",\
-\"#a16205\",\"#f5efcd\",\"#b98361\",\"#990b3a\",\"#cc561b\",\"#e0a100\",\"#b98361\",\"#ae3a34\",\
+set_attribute use-theme-colors false;
+set_attribute background-color \#221107;
+set_attribute foreground-color \#f5efcd;
+set_attribute cursor-background-color \#f5efcd;
+set_attribute cursor-foreground-color \#221107;
+set_attribute highlight-background-color \#f5efcd;
+set_attribute highlight-foreground-color \#221107;
+set_attribute palette [\"#221107\",\"#990b3a\",\"#cc561b\",\"#e0a100\",\
+\"#b98361\",\"#ae3a34\",\"#a16205\",\"#f5efcd\",\"#b98361\",\"#990b3a\",\
+\"#cc561b\",\"#e0a100\",\"#b98361\",\"#ae3a34\",\
 \"#a16205\",\"#f5efcd\"];
--- ports/linux/flamerial.sh
+++ ports/linux/flamerial.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-function SetColor()
-{
-    echo -ne \\033]P${1}${2};
+set_color() {
+  echo -ne \\033]P${1}${2};
 }
 
 if [[ ${TERM} != linux ]];
@@ -11,19 +10,19 @@ then
     exit 1;
 fi
 
-SetColor 0 221107;
-SetColor 1 990b3a;
-SetColor 2 cc561b;
-SetColor 3 e0a100;
-SetColor 4 b98361;
-SetColor 5 ae3a34;
-SetColor 6 a16205;
-SetColor 7 f5efcd;
-SetColor 8 b98361;
-SetColor 9 990b3a;
-SetColor a cc561b;
-SetColor b e0a100;
-SetColor c b98361;
-SetColor d ae3a34;
-SetColor e a16205;
-SetColor f f5efcd;
+set_color 0 221107;
+set_color 1 990b3a;
+set_color 2 cc561b;
+set_color 3 e0a100;
+set_color 4 b98361;
+set_color 5 ae3a34;
+set_color 6 a16205;
+set_color 7 f5efcd;
+set_color 8 b98361;
+set_color 9 990b3a;
+set_color a cc561b;
+set_color b e0a100;
+set_color c b98361;
+set_color d ae3a34;
+set_color e a16205;
+set_color f f5efcd;
