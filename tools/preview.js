28e9db6fd2ee7c2e76bfbf7a87b95a82723d9cda
Sherman Rofeman <skippyr.developer@icloud.com>
2024-12-07 14:35:39 -0300

    Update script naming

--- tools/preview.js
+++ tools/preview.js
@@ -1,11 +1,11 @@
-function writeTribalDecoration(length) {
-  for (let offset = 0; offset < length; ++offset) {
-    process.stdout.write(offset % 2 ? '\x1b[31mv' : '\x1b[33m≥');
+function writeTribalDecor(len) {
+  for (let i = 0; i < len; ++i) {
+    process.stdout.write(i % 2 ? '\x1b[31mv' : '\x1b[33m≥');
   }
   process.stdout.write('\x1b[39m');
 }
 
-function writeColumnDecoration(isOpen, isTop) {
+function writeColDecor(isOpen, isTop) {
   process.stdout.write(isOpen ? isTop ? '\x1b[35m¦/:\x1b[39m '
                                       : '\x1b[35m¦\\:\x1b[39m '
                                       :
@@ -13,30 +13,30 @@ function writeColumnDecoration(isOpen, isTop) {
                                       : ' \x1b[35m:/¦\x1b[39m')
 }
 
-function writePalette(isBackground) {
-  for (let ansiColor = 1; ansiColor < 9; ++ansiColor) {
-    process.stdout.write(isBackground ? `\x1b[48;5;${ansiColor}m   \x1b[49m`
-                                      : `\x1b[38;5;${ansiColor}mFla\x1b[39m`);
+function writePalette(isBg) {
+  for (let i = 1; i < 9; ++i) {
+    process.stdout.write(isBg ? `\x1b[48;5;${i}m   \x1b[49m`
+                              : `\x1b[38;5;${i}mFla\x1b[39m`);
   }
 }
 
 console.log();
 process.stdout.write('   ');
-writeTribalDecoration(10);
+writeTribalDecor(10);
 process.stdout.write(' Flamerial ');
-writeTribalDecoration(11);
+writeTribalDecor(11);
 console.log();
 process.stdout.write('   ');
-writeColumnDecoration(true, true);
+writeColDecor(true, true);
 writePalette(true);
-writeColumnDecoration(false, true);
+writeColDecor(false, true);
 console.log();
 process.stdout.write('   ');
-writeColumnDecoration(true, false);
+writeColDecor(true, false);
 writePalette(false);
-writeColumnDecoration(false, false);
+writeColDecor(false, false);
 console.log();
 process.stdout.write('   ');
-writeTribalDecoration(32);
+writeTribalDecor(32);
 console.log();
 console.log();
