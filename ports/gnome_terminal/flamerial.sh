3425c6451cce6c215a8ba1fc063447e09316226f
Sherman Rofeman <skippyr.developer@icloud.com>
2024-12-09 22:17:26 -0300

    fixes

--- ports/gnome_terminal/flamerial.sh
+++ ports/gnome_terminal/flamerial.sh
@@ -2,18 +2,18 @@
 
 uuid=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \');
 
-set_attr() {
+set_attribute() {
   gsettings set \
       org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${uuid}/ \
       ${1} ${2};
 }
 
-set_attr use-theme-colors false;
-set_attr background-color \#150d06;
-set_attr foreground-color \#e9d397;
-set_attr cursor-background-color \#e9d397;
-set_attr cursor-foreground-color \#150d06;
-set_attr highlight-background-color \#e9d397;
-set_attr highlight-foreground-color \#150d06;
-set_attr palette [\"#150d06\",\"#b31c13\",\"#855814\",\"#c76800\",\"#b08548\",\"#8a3820\",\"#a83e00\",\"#e9d397\",\"#695f44\",\"#b31c13\",\"#855814\",\"#c76800\",\"#b08548\",\
+set_attribute use-theme-colors false;
+set_attribute background-color \#150d06;
+set_attribute foreground-color \#e9d397;
+set_attribute cursor-background-color \#e9d397;
+set_attribute cursor-foreground-color \#150d06;
+set_attribute highlight-background-color \#e9d397;
+set_attribute highlight-foreground-color \#150d06;
+set_attribute palette [\"#150d06\",\"#b31c13\",\"#855814\",\"#c76800\",\"#b08548\",\"#8a3820\",\"#a83e00\",\"#e9d397\",\"#695f44\",\"#b31c13\",\"#855814\",\"#c76800\",\"#b08548\",\
 \"#8a3820\",\"#a83e00\",\"#e9d397\"];
