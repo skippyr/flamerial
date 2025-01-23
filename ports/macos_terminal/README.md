ecd09fd5ae0c879d33d940ce9ea58a0e37c102b9
Sherman Rofeman <skippyr.developer@icloud.com>
2025-01-22 21:51:43 -0300

    add port for macOS Terminal

---
+++ ports/macos_terminal/README.md
@@ -0,0 +1,12 @@
+<p align="center">
+  <img alt="" src="../../assets/images/ornament.png" width=1020 />
+</p>
+<h1 align="center">≥v≥v&ensp;Flamerial&ensp;≥v≥v</h1>
+<p align="center">— macOS Terminal —</p>
+
+## ❡ Install
+- Open the macOS Terminal settings.
+- Select `Profile > Text > ... > Import...`.
+- Open `Flamerial.terminal`.
+- Select the `Flamerial` theme and hit `Default`.
+- Reload the macOS Terminal.
--- scripts/preview.rb
+++ scripts/preview.rb
@@ -11,15 +11,17 @@ def set_clr(clr, lyr)
   print("\e[#{$lyrs[lyr]}8;2;#{clr[:r]};#{clr[:g]};#{clr[:b]}m")
 end
 
-def reset_clrs
-  print("\e[39;49m")
+def reset_clr(lyr)
+  print("\e[#{$lyrs[lyr]}9m")
 end
 
 def write_blank
   2.times {
+    set_clr(:black, :bg)
     42.times {
       print(' ')
     }
+    reset_clr(:bg)
     puts
   }
 end
@@ -56,25 +58,36 @@ $lyrs = {
   :bg => 4
 }
 
-set_clr(:black, :bg)
 write_blank
+set_clr(:black, :bg)
 print('     ')
 write_tribal_decor(10)
 set_clr(:white, :fg)
 print(' Flamerial ')
 write_tribal_decor(11)
 set_clr(:white, :fg)
-puts('     ')
+print('     ')
+reset_clr(:bg)
+puts
+set_clr(:black, :bg)
 print('     ¦/: ')
 write_pal(:bg)
 set_clr(:black, :bg)
-puts(' :\\¦     ')
+print(' :\\¦     ')
+reset_clr(:bg)
+puts
+set_clr(:black, :bg)
 print('     ¦\\: ')
 write_pal(:fg)
 set_clr(:white, :fg)
-puts(' :/¦     ')
+print(' :/¦     ')
+reset_clr(:bg)
+puts
+set_clr(:black, :bg)
 print('     ')
 write_tribal_decor(32)
-puts('     ')
+print('     ')
+reset_clr(:bg)
+puts
 write_blank
-reset_clrs
+reset_clr(:fg)
