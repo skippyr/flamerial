ec311fd3658efc7b5442d46e6f0eda5fb3dd6437
Sherman Rofeman <skippyr.developer@icloud.com>
2025-02-02 19:36:56 -0300

    add gnome-terminal port

---
+++ ports/gnome_terminal/README.md
@@ -0,0 +1,13 @@
+<p align="center">
+  <img alt="" src="../../assets/images/ornament.png" width=1020 />
+</p>
+<h1 align="center">≥v≥v&ensp;Flamerial&ensp;≥v≥v</h1>
+<p align="center">—&ensp;Gnome Terminal&ensp;—</p>
+
+## ❡ Install
+### Procedures
+- Using the Gnome Terminal, run the script `flamerial.sh`.
+
+```zsh
+./flamerial.sh;
+```
---
+++ ports/gnome_terminal/flamerial.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+uuid=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \');
+
+set_attribute() {
+	gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${uuid}/ ${1} ${2};
+}
+
+set_attribute use-theme-colors false;
+set_attribute background-color \#030101;
+set_attribute foreground-color \#e8cc7d;
+set_attribute cursor-background-color \#e8cc7d;
+set_attribute cursor-foreground-color \#030101;
+set_attribute highlight-background-color \#e8cc7d;
+set_attribute highlight-foreground-color \#030101;
+set_attribute palette [\"#030101\",\"#991f06\",\"#7d0800\",\"#c76800\",\"#b08548\",\"#8a3820\",\"#a83e00\",\"#e8cc7d\",\"#695f44\",\"#991f06\",\"#7d0800\",\"#c76800\",\"#b08548\",\"#8a3820\",\"#a83e00\",\"#e8cc7d\"];
